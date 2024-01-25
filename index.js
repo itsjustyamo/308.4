ec826ac3135652256f15706d639b9904b5c174f8
Tshiamo Tsoku <ttsoku@icloud.com>
2024-01-25 14:41:57 -0800

    please go

---
+++ index.js
@@ -0,0 +1,20 @@
+//Part One
+let info = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
+
+// let array = info.slice("\n");
+
+// console.log(array);
+
+// Part Two
+//Comment out line 4 and 6 for this to work
+
+let array = info.split("\n")
+
+let infoGroup = [];
+
+for (let i = 0; i < array.length; i++){
+
+    infoGroup.push(array[i].split(","));
+    
+}
+console.log(infoGroup);
